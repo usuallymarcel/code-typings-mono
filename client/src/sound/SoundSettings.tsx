73dc0ec933b512264ad83179fb51b3a4aca9c9af
Marcel van Eijk <marcelve2112@gmail.com>
2026-02-02 19:55:46 +1300

    sound settings icons and hidden menu

--- client/src/sound/SoundSettings.tsx
+++ client/src/sound/SoundSettings.tsx
@@ -1,10 +1,11 @@
 // SoundSettings.tsx
 import { useSound } from './useSound'
 import type { SoundEvent } from './soundEvent'
-// import { useState } from 'react'
+import { useState } from 'react'
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import { GoMute, GoUnmute } from "react-icons/go";
 
 const events: SoundEvent[] = ['type', 'error', 'oops', 'pop', 'paper']
-// const [hide, setHide] = useState<boolean>(false)
 
 
 export function SoundSettings() {
@@ -15,31 +16,41 @@ export function SoundSettings() {
         toggleMute, 
     } = useSound()
 
+    const [hide, setHide] = useState<boolean>(false)
+
+
     return (
         <div className='p-4 bg-neutral-800 rounded'>
-            <h2 className='text-lg mb-2'>Sound Settings</h2>
-            {/* <button onClick={() => setHide(prev => !prev)}>
-                {hide ? 'Unhide' : 'Hide'}
-            </button> */}
+            <div className="flex items-center justify-between" onClick={() => setHide(prev => !prev)}>
 
-            <button onClick={toggleMute}>
-                {muted ? 'Unmute' : 'Mute'}
-            </button>
 
-            {events.map(event => (
-            <div key={event} className='mt-3'>
-                <strong>{event}</strong>
-
-                <input
-                type='range'
-                min={0}
-                max={1}
-                step={0.05}
-                value={sounds[event].volume}
-                onChange={e => setVolume(event, Number(e.target.value))}
-                />
+                <div className="flex gap-2">
+                    {hide ? <IoIosArrowForward /> : <IoIosArrowDown />}
+                </div>
+                <h2 className='text-lg'>Sound Settings</h2>
             </div>
-            ))}
+
+            {!hide && (
+                <div className="mt-4">
+                    {events.map(event => (
+                    <div key={event} className='mt-3'>
+                        <strong>{event}</strong>
+
+                        <input
+                        type='range'
+                        min={0}
+                        max={1}
+                        step={0.05}
+                        value={sounds[event].volume}
+                        onChange={e => setVolume(event, Number(e.target.value))}
+                        />
+                    </div>
+                    ))}
+                    <button onClick={toggleMute} className="mt-4">
+                        {!muted ? <GoUnmute /> : <GoMute />}
+                    </button>
+                </div>
+            )}
         </div>
     )
 }

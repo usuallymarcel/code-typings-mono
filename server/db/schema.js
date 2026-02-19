f2a689fc695f4416aeb5deaed1a5de895e340d02
usuallymarcel <marcelve2112@gmail.com>
2026-02-19 23:01:40 +1300

    start of sqlite db

---
+++ server/db/schema.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3'
+import { execute } from './sql'
+
+const db = new sqlite3.Database('typings.db', sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.log(err.message)
+})
+
+async function createTable() {
+    const sql = `CREATE TABLE IF NOT EXISTS text (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+        text TEXT NOT NULL
+    )`
+    try {
+        execute(db, sql)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        db.close()
+    }
+}
+
+export async function initialiseDb() {
+    await createTable()
+}
+
+process.on('exit', () => {
+    db.close((err) => {
+        if (err) console.error(err.message)
+    })
+})
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+import { initialiseDb } from './db/schema.js';
 
 require('dotenv').config();
 
@@ -8,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+await initialiseDb()
 
 // app.use(express.static(path.resolve(__dirname, '../client/dist')));
 

8d54f928ee1b9e49bc5ddcf53e45345dc8d485b9
usuallymarcel <mv119@students.waikato.ac.nz>
2026-02-22 20:58:22 +1300

    mount routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import { initialiseDb } from './db/schema.js'
+import { initialiseDb } from './db/textdb.js'
 import dotenv from 'dotenv'
-import getText from './routes/text.js'
+import {getText, getTexts} from './routes/text.js'
+import upload from './routes/upload.js'
+import uploadMiddleware from './middleware/upload.js'
 
 dotenv.config()
 
@@ -16,11 +18,15 @@ await initialiseDb()
 
 // app.use(express.static(path.resolve(__dirname, '../client/dist')))
 
-app.get('/text', (_req, res) => {
-    res.json({ message: "This is longer sample text coming from the server to make sure that the front is equipped to handle such a case blah blah blah this is sample text from server"})
-    // res.json({ message: "this is sample text from server"})
-})
+// app.get('/text', (_req, res) => {
+//     res.json({ message: "This is longer sample text coming from the server to make sure that the front is equipped to handle such a case blah blah blah this is sample text from server"})
+//     // res.json({ message: "this is sample text from server"})
+// })
 
-app.get('/piss', getText)
+app.post('/upload', uploadMiddleware.single("file"), upload)
+
+app.get('/text', getText)
+
+app.get('/texts', getTexts)
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`))

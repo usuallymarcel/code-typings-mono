38b3d9ce255afed0790930ac9b8f741373d065c6
usuallymarcel <marcelve2112@gmail.com>
2026-05-25 23:08:13 +1200

    add pet sprite sheet for example pet

--- client/src/modules/pets/assets/cat/metadata.ts
+++ client/src/modules/pets/assets/cat/metadata.ts
@@ -1,9 +1,11 @@
 import idle from './idle.png'
 import walk from './walk.png'
 import sleep from './sleep.png'
+import follow from './follow.png'
 
-export const catAssets = {
+export const petAssets = {
     idle,
     walk,
     sleep,
+    follow
 }
